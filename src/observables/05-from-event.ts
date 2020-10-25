43b1ecaca9748d3fe4b2e28eed90707941e98176
Angel Salvador <angelsalvadordev@gmail.com>
2020-10-25 13:59:39 -0500

    fromEvent

---
+++ src/observables/05-from-event.ts
@@ -0,0 +1,15 @@
+import { fromEvent } from "rxjs";
+
+// DOM Events
+
+const src1$ = fromEvent<MouseEvent>(document, "click");
+const src2$ = fromEvent<KeyboardEvent>(document, "keyup");
+
+const observer = {
+  next: (val) => console.log("[NEXT]: ", val),
+};
+
+src1$.subscribe(({ x, y }) => {
+  console.log(x, y);
+});
+src2$.subscribe((event) => console.log(event.key));
